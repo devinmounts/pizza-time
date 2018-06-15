7bc20fb8a111dd855af416aed72405d7ec192003
Devin Mounts <devinmounts@gmail.com>
2018-06-15 10:26:31 -0700

    create doc ready jquery, no values are grabbed.

--- js/scripts.js
+++ js/scripts.js
@@ -12,18 +12,18 @@ Pizza.prototype.name = function () {
 Pizza.prototype.size = function (size, price) {
   if (this.size === "sm"){
     this.price += 8;
-  }else if (this.size === "med") {
-    this.price += 10;
-  }else if (this.size === "lg") {
-    this.price  += 15;
-  }else if (this.size === "xl") {
-    this.price += 18;
+  // }else if (this.size === "med") {
+  //   this.price += 10;
+  // }else if (this.size === "lg") {
+  //   this.price  += 15;
+  // }else if (this.size === "xl") {
+  //   this.price += 18;
   }else
   return this.price;
 }
 
-Pizza.prototype.toppings = function (toppings, price) {
-  for(var i=0, i<toppings.length, i++)
+Pizza.prototype.toppings = function() {
+  for (var i=0; i = toppings.length; i++) {
     if (this.toppings === 1){
       this.price += 0;
     }else if (this.toppings > 0 && this.toppings <= 3) {
@@ -32,8 +32,24 @@ Pizza.prototype.toppings = function (toppings, price) {
       cost = 5;
     }else
     return cost
+  }
 }
 
-Pizza.prototype.price = function (cost) {
-  return this.
+Pizza.prototype.price = function () {
+  return this.price
 }
+
+
+$(function(){
+  var pizza = new Pizza()
+
+  $("#submitOrder").submit(function(e){
+  e.preventDefault();
+  console.log("here");
+  pizza.name = $("#inputName").val();
+  pizza.size = $("#inputSize").val();
+  console.log(pizza.name);
+  console.log(pizza.size);
+  $(".showOrder").text()
+  });
+});
