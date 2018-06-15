521d5f050003a1bfaae3feeffdfc86673d6ef0a8
Devin Mounts <devinmounts@gmail.com>
2018-06-15 09:34:38 -0700

    create pizza object, name prototype and size prototype.

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+function Pizza (name, price, size, toppings){
+  this.name = name;
+  this.price = 0;
+  this.size = size;
+  this.toppings = [];
+}
+
+Pizza.prototype.name = function () {
+  return this.name;
+}
+
+Pizza.prototype.size = function (size, price) {
+  if (this.size === "sm"){
+    this.price += 8;
+  }else if (this.size === "med") {
+    this.price += 10;
+  }else if (this.size === "lg") {
+    this.price  += 15;
+  }else if (this.size === "xl") {
+    this.price += 18;
+  }else
+  return this.price;
+}
+
+Pizza.prototype.toppings = function (toppings, price) {
+  for(var i=0, i<toppings.length, i++)
+    if (this.toppings === 1){
+      this.price += 0;
+    }else if (this.toppings > 0 && this.toppings <= 3) {
+      cost = 3;
+    }else if (this.toppings >= 5) {
+      cost = 5;
+    }else
+    return cost
+}
+
+Pizza.prototype.price = function (cost) {
+  return this.
+}
